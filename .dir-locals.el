e5ffc6e28d21b7336cd6eb7ecaf14e5a68102489
TrevorWilsonSS <trevor.wilson@starbucksoftware.com>
2019-02-27 07:36:43 -0700

    Updated .dir-locals with current build command

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((projectile-project-compilation-cmd . "bazel build --experimental_cc_skylark_api_enabled_packages=@rules_foreign_cc//tools/build_defs,tools/build_defs,@foreign_cc_impl //src:test_libsodium")
+((nil . ((projectile-project-compilation-cmd . "bazel build --experimental_cc_skylark_api_enabled_packages=@rules_foreign_cc//tools/build_defs,tools/build_defs,@foreign_cc_impl //src:main --verbose_failures --sandbox_debug")
          ))
  )
--- dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((projectile-project-compilation-cmd . "bazel build --experimental_cc_skylark_api_enabled_packages=@rules_foreign_cc//tools/build_defs,tools/build_defs,@foreign_cc_impl //src:main")
-         ))
- )
